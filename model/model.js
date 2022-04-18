1cd4828a9d7bf790cba736b68c71db319f319acc
Pierce Issah <PJIceskull2003@gmail.com>
2022-04-18 12:56:57 -0400

    Added home html code to homeContent

--- model/model.js
+++ model/model.js
@@ -1,5 +1,33 @@
 // variables
-var homeContent = `<h1>HOME</h1>`;
+var homeContent = ` <!-- home image -->
+      <div class="home-img">
+        <h1>Header goes here</h1>
+        <h2>Less important text goes here.</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat
+          aliquid minus nemo sed est.
+        </p>
+        <button class="readMoreBTN">READ MORE</button>
+      </div>
+      <!-- Quote Section -->
+      <div class="quote-Section">
+        <h3>
+          “Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab
+          illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+          explicabo. Iste natus error sit voluptatem accusantium doloremque
+          laudantium totam rem aperiam eaque ipsa quae ab illo inventore
+          veritatis et quasi architecto beatae vitae dicta sunt explicabo”
+        </h3>
+        <p class="author-Text">John Smith</p>
+        <p class="jobTitle-Text">Corporation CEO, books author.</p>
+      </div>
+      <!-- Upcoming Events -->
+      <div class="event-Section">
+        <h1>Upcoming Events</h1>
+        <hr />
+        <div class="date-Section"></div>
+      </div>`;
 var aboutContent = `<h1>ABOUT</h1>`;
 var galleryContent = `<h1>GALLERY</h1>`;
 var blogContent = `<h1>BLOG</h1>`;
