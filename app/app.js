0d2e44a16a66f402b3bfd50f6a48b5cfc095c3d5
Pierce Issah <PJIceskull2003@gmail.com>
2022-04-18 11:39:30 -0400

    Added listeners& route function to app.js

--- app/app.js
+++ app/app.js
@@ -1,5 +1,25 @@
+// importing files from model.js
 import * as MODEL from "../model/model.js";
 
+function route() {
+    let hashTag = window.location.hash;
+    let pageName = hashTag.replace("#", "");
+    let pageContent = pageName + "Content";
+
+    if (pageName == "") {
+        pageContent = "homeContent";
+    }
+
+    MODEL.modelPageName(pageContent);
+}
+
+// Listens and write console.log when something is pressed
+function initListeners() {
+    $(window).on("hashchange", route)
+}
+
 $(document).ready(function () {
-    console.log("Pierce");
+    // console.log("Pierce");
+    // console.log(MODEL.myName)
+    initListeners();
 });
