3c5f4a288c78dd9b1f1dd0aa00ad1216fb3a0fa0
Pierce Issah <PJIceskull2003@gmail.com>
2022-04-18 12:48:05 -0400

    add attr to change source of nav image

--- model/model.js
+++ model/model.js
@@ -15,9 +15,13 @@ export function modelPageName(pageName) {
     if (pageName == "homeContent") {
         $("nav a").removeClass("blackText").addClass("whiteText");
         $("hr").removeClass("black-hr").addClass("white-hr");
+        // Change image in nav src using attr
+        $("nav img").attr("src", "images/logo/logo-white.svg");
     } else {
         $("nav a").removeClass("whiteText").addClass("blackText");
         $("hr").removeClass("white-hr").addClass("black-hr");
+        // Change image src in nav using attr
+        $("nav img").attr("src", "images/logo/logo-black.svg")
     }
 
     // try & catch to catch any errors
