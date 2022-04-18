6178996af1207d60a754de79969e5ff3ef5dc2fc
Pierce Issah <PJIceskull2003@gmail.com>
2022-04-18 11:42:54 -0400

    add export function to target and change content

---
+++ model/model.js
@@ -0,0 +1,31 @@
+// variables
+var homeContent = `<h1>HOME</h1>`;
+var aboutContent = `<h1>ABOUT</h1>`;
+var galleryContent = `<h1>GALLERY</h1>`;
+var blogContent = `<h1>BLOG</h1>`;
+var contactContent = `<h1>CONTACT</h1>`;
+
+export var myName = "Pierce";
+
+// 
+// export function modelPageName(pgName)
+export function modelPageName(pageName) {
+
+    // replace font color for nav a on homepage
+    if (pageName == "homeContent") {
+        $("nav a").removeClass("blackText").addClass("whiteText");
+        $("hr").removeClass("black-hr").addClass("white-hr");
+    } else {
+        $("nav a").removeClass("whiteText").addClass("blackText");
+        $("hr").removeClass("white-hr").addClass("black-hr");
+    }
+
+    // try & catch to catch any errors
+    try {
+        eval(pageName);
+        $("#app").html(eval(pageName))
+    }
+    catch (e) {
+        console.log("hello", e)
+    }
+}
