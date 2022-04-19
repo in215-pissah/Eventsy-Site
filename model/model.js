b9eebb517ed6588242322800ffa46568e8402572
Pierce Issah <PJIceskull2003@gmail.com>
2022-04-19 10:28:18 -0400

    Inserted html code into homeContent

--- model/model.js
+++ model/model.js
@@ -1,5 +1,5 @@
 // variables
-var homeContent = ` <!-- home image -->
+var homeContent = `<!-- home image -->
       <div class="home-img">
         <h1>Header goes here</h1>
         <h2>Less important text goes here.</h2>
@@ -24,9 +24,79 @@ var homeContent = ` <!-- home image -->
       </div>
       <!-- Upcoming Events -->
       <div class="event-Section">
-        <h1>Upcoming Events</h1>
-        <hr />
-        <div class="date-Section"></div>
+        <div class="quote-Title">
+          <h1>Upcoming Events</h1>
+          <hr />
+        </div>
+
+        <div class="date-Section">
+          <!-- event Boxes -->
+          <!-- box1 -->
+          <div class="event-Box">
+            <div class="date-Box">
+              <p>
+                <span class="dateNumber">06</span>
+                <span class="dateMonth">JUN</span>
+              </p>
+            </div>
+            <div class="date-Text">
+              <h3>Sed et persipiatis unde omnis iste natus</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <!-- box2 -->
+          <div class="event-Box">
+            <div class="date-Box">
+              <p>
+                <span class="dateNumber">30</span>
+                <span class="dateMonth">JUL</span>
+              </p>
+            </div>
+            <div class="date-Text">
+              <h3>Sed et persipiatis unde omnis iste natus</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <!-- box3 -->
+          <div class="event-Box">
+            <div class="date-Box">
+              <p>
+                <span class="dateNumber">30</span>
+                <span class="dateMonth">AUG</span>
+              </p>
+            </div>
+            <div class="date-Text">
+              <h3>Sed et persipiatis unde omnis iste natus</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <!-- box4 -->
+          <div class="event-Box">
+            <div class="date-Box">
+              <p>
+                <span class="dateNumber">23</span>
+                <span class="dateMonth">NOV</span>
+              </p>
+            </div>
+            <div class="date-Text">
+              <h3>Sed et persipiatis unde omnis iste natus</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <!-- box5 -->
+          <div class="event-Box">
+            <div class="date-Box">
+              <p>
+                <span class="dateNumber">23</span>
+                <span class="dateMonth">DEC</span>
+              </p>
+            </div>
+            <div class="date-Text">
+              <h3>Sed et persipiatis unde omnis iste natus</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
       </div>`;
 var aboutContent = `<h1>ABOUT</h1>`;
 var galleryContent = `<h1>GALLERY</h1>`;
@@ -39,25 +109,25 @@ export var myName = "Pierce";
 // export function modelPageName(pgName)
 export function modelPageName(pageName) {
 
-    // replace font color for nav a on homepage
-    if (pageName == "homeContent") {
-        $("nav a").removeClass("blackText").addClass("whiteText");
-        $("hr").removeClass("black-hr").addClass("white-hr");
-        // Change image in nav src using attr
-        $("nav img").attr("src", "images/logo/logo-white.svg");
-    } else {
-        $("nav a").removeClass("whiteText").addClass("blackText");
-        $("hr").removeClass("white-hr").addClass("black-hr");
-        // Change image src in nav using attr
-        $("nav img").attr("src", "images/logo/logo-black.svg")
-    }
+  // replace font color for nav a on homepage
+  if (pageName == "homeContent") {
+    $("nav a").removeClass("blackText").addClass("whiteText");
+    $("hr").removeClass("black-hr").addClass("white-hr");
+    // Change image in nav src using attr
+    $("nav img").attr("src", "images/logo/logo-white.svg");
+  } else {
+    $("nav a").removeClass("whiteText").addClass("blackText");
+    $("hr").removeClass("white-hr").addClass("black-hr");
+    // Change image src in nav using attr
+    $("nav img").attr("src", "images/logo/logo-black.svg")
+  }
 
-    // try & catch to catch any errors
-    try {
-        eval(pageName);
-        $("#app").html(eval(pageName))
-    }
-    catch (e) {
-        console.log("hello", e)
-    }
+  // try & catch to catch any errors
+  try {
+    eval(pageName);
+    $("#app").html(eval(pageName))
+  }
+  catch (e) {
+    console.log("hello", e)
+  }
 }
