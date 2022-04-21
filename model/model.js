e02d4ebdaac1d27170be1a9130fc93a720aeb153
Pierce Issah <PJIceskull2003@gmail.com>
2022-04-21 18:39:25 -0400

    Add html code to AboutContent

--- model/model.js
+++ model/model.js
@@ -98,7 +98,25 @@ var homeContent = `<!-- home image -->
           </div>
         </div>
       </div>`;
-var aboutContent = `<h1>ABOUT</h1>`;
+var aboutContent = ` <div class="wrapper">
+        <div class="about-img"></div>
+        <h1>
+          OUR HISTORY:
+          <hr class="black-hr" />
+        </h1>
+
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem.
+        </p>
+      </div>`;
 var galleryContent = `<h1>GALLERY</h1>`;
 var blogContent = `<h1>BLOG</h1>`;
 var contactContent = `<h1>CONTACT</h1>`;
